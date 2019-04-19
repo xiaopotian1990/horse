326e7c89c4764b1b1f6b2901d45e3d3f83d624ce
zoulu <zoulu669064@live.com>
2019-04-19 17:44:13 +0800

    sql

---
+++ doc/db/schema.sql
@@ -0,0 +1,11 @@
+-- horse 核心表
+create database `horse` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- horse 工作流相关库
+create database `horse_ac` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- horse zipkin相关库
+create database `horse_zipkin` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- horse 任务相关库
+create database `horse_job` default character set utf8mb4 collate utf8mb4_general_ci;
